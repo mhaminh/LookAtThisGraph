b893da63938196f24aea72b4ab2f75bf134d5613
Martin Ha Minh <haminh@mpp.mpg.de>
2020-06-30 17:39:54 +0200

    Restructure Dataset and use i3cols_dataloader

--- lookatthisgraph/utils/dataset.py
+++ lookatthisgraph/utils/dataset.py
@@ -1,10 +1,13 @@
 import logging
+from importlib.resources import path
 import numpy as np
-from joblib import Parallel, delayed
+# from joblib import Parallel, delayed
 import torch
+import lookatthisgraph.resources
 from torch_geometric.data import Data, DataLoader
-from lookatthisgraph.utils.datautils import get_input_data, time_to_position, get_dom_xyz, get_edge_information, filter_pulses, build_data_list
+from lookatthisgraph.utils.datautils import process_charges, build_data_list
 from lookatthisgraph.utils.pulsenormalizer import PulseNormalizer
+from lookatthisgraph.utils.i3cols_dataloader import load_events
 
 
 class Dataset(object):
@@ -13,75 +16,45 @@ class Dataset(object):
 
     def __init__(self, config):
         self.files = config['file_list']
-        self.input_labels = ['energy', 'zenith']
         self.training_labels = config['training_labels']
-        if 'n_jobs' in config:
-            self.n_jobs = config['n_jobs']
-        else:
-            self.n_jobs = 1
-        if 'max_pulses' in config:
-            self.max_pulses = config['max_pulses']
-        else:
-            self.max_pulses = float('inf')
-
         self.include_charge = config['include_charge']
+        logging.basicConfig(level=logging.INFO)
+        logging.info('Loading events')
+        with path(lookatthisgraph.resources, 'geo_array.npy') as p:
+            file_input = load_events(self.files, geo=p)
 
-        raw_pulses, raw_truths = get_input_data(self.files, self.input_labels, self.n_jobs)
-        self.filtered_pulses, self.filtered_truths, self.filter_mask = \
-                filter_pulses(raw_pulses, raw_truths, self.n_jobs)
+        logging.debug('Preprocessing events')
+        raw_pulses = [event['hits'] for event in file_input[0]]
+        raw_truths = [event['params'] for event in file_input[0]]
+        self.input_dict = {key: idx for idx, key in enumerate(file_input[1])}
 
-        self._n_events = len(self.filtered_pulses)
-        logging.info('%i events' % self._n_events)
+        empty_mask = [i for i, ev in enumerate(raw_pulses) if len(ev) > 0]
+        self.raw_pulses = [raw_pulses[i][:, :5] for i in empty_mask]
+        self.raw_truths = [raw_truths[i] for i in empty_mask]
 
-        transformed_truths = self._transform_truths()
+        self.n_events = len(self.raw_pulses)
+        logging.info('%i events' % self.n_events)
 
-        self.dom_xyz = get_dom_xyz(self.filtered_pulses, 0)
+        logging.debug('Transforming truths')
+        self.transformed_truths = self._transform_truths()
+
+        logging.debug('Start normalizing pulses')
         normalized_features = self._get_normalized_features()
 
-        self._data_list = build_data_list(
+        self.data_list = build_data_list(
             normalized_features,
-            transformed_truths
+            self.transformed_truths
         )
 
         self.reshuffle()
 
 
     def reshuffle(self):
-        self._permutation = np.random.permutation(self._n_events)
-
-
-    def get_loaders(self, batch_size, train_split, test_split, val_split='batch'):
-        split = lambda s: int(self._n_events * s) if s < 1 else s
-
-        if val_split == 'batch':
-            n_val = batch_size
-        else:
-            n_val = split(val_split)
-        n_train, n_test = split(train_split), split(test_split)
-        if n_train + n_val + n_test > self._n_events:
-            raise ValueError('Loader configuration exceeds number of data samples')
-        dataset_shuffled = [self._data_list[i] for i in self._permutation]
-
-        train_loader = DataLoader(dataset_shuffled[:n_train], batch_size=batch_size, drop_last=True, shuffle=True)
-        val_loader = DataLoader(dataset_shuffled[n_train:n_train+n_val], batch_size=batch_size, drop_last=True)
-        test_loader = DataLoader(dataset_shuffled[n_train+n_val:n_train], batch_size=batch_size, drop_last=True)
-
-        return train_loader, val_loader, test_loader
+        self.permutation = np.random.permutation(self.n_events)
 
 
     def _get_normalized_features(self):
-        if self.include_charge:
-            features = [np.concatenate((np.log10(event[:, 2].reshape(-1, 1)), # log charges
-                                        event[:, 1].reshape(-1, 1), # Time
-                                        doms, # xyz
-                                       ), axis=1)
-                        for event, doms in zip(self.filtered_pulses, self.dom_xyz)]
-        else:
-            features = [np.concatenate((event[:, 1].reshape(-1, 1), # Time
-                                        doms, # xyz
-                                       ), axis=1)
-                        for event, doms in zip(self.filtered_pulses, self.dom_xyz)]
-
+        features = [process_charges(event, self.include_charge) for event in self.raw_pulses]
         pn = PulseNormalizer(features)
         features_normalized = pn.normalize(mode='gauss')
         return features_normalized
@@ -89,11 +62,11 @@ class Dataset(object):
 
     def _transform_truths(self):
         if self.training_labels == ['zenith']:
-            col = np.where(self.input_labels == 'zenith')[0]
-            transformed_truths = [[np.sin(y[col]), np.cos(y[col])] for y in self.filtered_truths]
+            col = self.input_dict['zenith']
+            transformed_truths = [[np.sin(y[col]), np.cos(y[col])] for y in self.raw_truths]
 
         elif self.training_labels == ['energy']:
-            col = np.where(self.input_labels == 'energy')[0]
-            transformed_truths = [np.log10(y[col]) for y in self.filtered_truths]
+            col = self.input_dict['neutrino_energy']
+            transformed_truths = [[np.log10(y[col])] for y in self.raw_truths]
 
         return transformed_truths
---
+++ lookatthisgraph/utils/i3cols_dataloader.py
@@ -0,0 +1,262 @@
+'''Module to read in i3cols format data
+i3cols is available here: https://github.com/jllanfranchi/i3cols
+'''
+import os
+import pkg_resources
+import numpy as np
+from tqdm.auto import tqdm
+
+def get_energies(mcprimary, mctree, mctree_idx, dtype=np.float32):
+    '''Get energies per event'''
+    
+    neutrino_energy = mcprimary['energy']
+    track_energy = np.zeros_like(neutrino_energy, dtype=dtype)
+    invisible_energy = np.zeros_like(neutrino_energy, dtype=dtype)
+    
+    for i in tqdm(range(len(mctree_idx)), desc='Calculating event energies'):
+        this_idx = mctree_idx[i]
+        this_mctree = mctree[this_idx['start'] : this_idx['stop']]
+        pdg = this_mctree['particle']['pdg_encoding']
+        en = this_mctree['particle']['energy']
+    
+        muon_mask = np.abs(pdg) == 13
+        if np.any(muon_mask):
+            track_energy[i] = np.max(en[muon_mask])
+
+        invisible_mask = (np.abs(pdg) == 12) | (np.abs(pdg) == 14) | (np.abs(pdg) == 16) 
+        # exclude primary:
+        invisible_mask[0] = False
+        if np.any(invisible_mask):
+            # we'll make the bold assumptions that none of the neutrinos re-interact ;)
+            invisible_energy[i] = np.sum(en[invisible_mask])
+
+    cascade_energy = neutrino_energy - track_energy - invisible_energy
+    return neutrino_energy, track_energy, cascade_energy
+
+def get_params(labels, mcprimary, mctree, mctree_idx, dtype=np.float32):
+    '''construct params array
+
+    retruns:
+    params : array shape (len(mcprimary), len(labels))
+    '''
+
+    neutrino_energy, track_energy, cascade_energy = get_energies(mcprimary, mctree, mctree_idx, dtype=dtype)
+    
+    params = np.empty(mcprimary.shape + (len(labels), ), dtype=dtype)
+
+    for i, label in enumerate(labels):
+        if label == 'x': params[:, i] = mcprimary['pos']['x']
+        elif label == 'y': params[:, i] = mcprimary['pos']['y']
+        elif label == 'z': params[:, i] = mcprimary['pos']['z']
+        elif label == 'time': params[:, i] = mcprimary['time']
+        elif label == 'azimuth': params[:, i] = mcprimary['dir']['azimuth']
+        elif label == 'zenith': params[:, i] = mcprimary['dir']['zenith']
+        elif label == 'neutrino_energy': params[:, i] = neutrino_energy
+        elif label == 'energy': params[:, i] = track_energy + cascade_energy
+        elif label == 'cascade_energy': params[:, i] = cascade_energy
+        elif label == 'track_energy': params[:, i] = track_energy
+
+    return params
+
+
+
+def load_charges(dir='/home/iwsatlas1/peller/work/oscNext/level7_v01.04/140000_i3cols',
+                 labels=['x', 'y', 'z', 'time', 'azimuth','zenith', 'cascade_energy', 'track_energy'],
+                 dtype=np.float32):
+    """
+    Create training data for chargenet
+    
+    Returns:
+    --------
+    total_charge : ndarray
+        shape (N_events, 2)
+    params : ndarray
+        shape (N_events, len(labels))
+    labels
+    """
+    
+    hits_idx = np.load(os.path.join(dir, 'SRTTWOfflinePulsesDC/index.npy'))
+    hits = np.load(os.path.join(dir, 'SRTTWOfflinePulsesDC/data.npy'))
+    mctree_idx = np.load(os.path.join(dir, 'I3MCTree/index.npy'))
+    mctree = np.load(os.path.join(dir, 'I3MCTree/data.npy'))
+    mcprimary = np.load(os.path.join(dir, 'MCInIcePrimary/data.npy'))
+
+    # Get charge per event
+    total_charge = np.zeros((hits_idx.shape[0], 2), dtype=dtype)
+    for i in range(len(hits_idx)):
+        this_idx = hits_idx[i]
+        this_hits = hits[this_idx['start'] : this_idx['stop']]
+
+        total_charge[i][0] = np.sum(this_hits['pulse']['charge'])
+        total_charge[i][1] = len(np.unique(this_hits['key']))
+        
+    params = get_params(labels, mcprimary, mctree, mctree_idx)
+    return total_charge, params, labels
+
+def load_strings(dir='/home/iwsatlas1/peller/work/oscNext/level7_v01.04/140000_i3cols',
+                 labels=['x', 'y', 'z', 'time', 'azimuth','zenith', 'cascade_energy', 'track_energy'],
+                 # geo=pkg_resources.resource_filename('freedom', 'resources/geo_array.npy'),
+                 geo = './resources/geo_array.npy',
+                 dtype=np.float32):
+    """
+    Create training data for hitnet
+    
+    Returns:
+    --------
+    string_charges : ndarray
+        shape (N_events*86, 5)
+    repeated_params : ndarray
+        shape (N_events*86, len(labels))
+    labels
+    """
+    
+    hits_idx = np.load(os.path.join(dir, 'SRTTWOfflinePulsesDC/index.npy'))
+    hits = np.load(os.path.join(dir, 'SRTTWOfflinePulsesDC/data.npy'))
+    mctree_idx = np.load(os.path.join(dir, 'I3MCTree/index.npy'))
+    mctree = np.load(os.path.join(dir, 'I3MCTree/data.npy'))
+    mcprimary = np.load(os.path.join(dir, 'MCInIcePrimary/data.npy'))
+
+    geo = np.load(geo)
+    
+    # constrcut strings array
+    # shape N x 86 x (x, y, min(z), q, string_idx)
+    
+    strings = np.zeros(hits_idx.shape + (86, 5,), dtype=dtype)
+    strings[:, :, 0:3] = geo[np.newaxis, :, -1]
+    strings[:, :, 4] = np.arange(86)[np.newaxis, :]
+
+    # Get charge per event and string
+    for i in range(len(hits_idx)):
+        this_idx = hits_idx[i]
+        this_hits = hits[this_idx['start'] : this_idx['stop']]
+        for hit in this_hits:
+            s_idx = hit['key']['string'] - 1
+            strings[i, s_idx, 3] += hit['pulse']['charge'] 
+
+    strings = strings.reshape(-1, 5)
+    
+    params = get_params(labels, mcprimary, mctree, mctree_idx)
+
+    repeated_params = np.repeat(params, repeats=86, axis=0)
+    
+    return strings, repeated_params, labels
+
+
+def load_hits(dir='/home/iwsatlas1/peller/work/oscNext/level7_v01.04/140000_i3cols',
+              labels=['x', 'y', 'z', 'time', 'azimuth','zenith', 'cascade_energy', 'track_energy'],
+              # geo=pkg_resources.resource_filename('freedom', 'resources/geo_array.npy'),
+              geo = './resources/geo_array.npy',
+              dtype=np.float32):
+    """
+    Create training data for hitnet
+    
+    Returns:
+    --------
+    single_hits : ndarray
+        shape (N_hits, 9)
+    repeated_params : ndarray
+        shape (N_hits, len(labels))
+    labels
+    """
+    
+    hits_idx = np.load(os.path.join(dir, 'SRTTWOfflinePulsesDC/index.npy'))
+    hits = np.load(os.path.join(dir, 'SRTTWOfflinePulsesDC/data.npy'))
+    mctree_idx = np.load(os.path.join(dir, 'I3MCTree/index.npy'))
+    mctree = np.load(os.path.join(dir, 'I3MCTree/data.npy'))
+    mcprimary = np.load(os.path.join(dir, 'MCInIcePrimary/data.npy'))
+
+    geo = np.load(geo)
+    
+    # constrcut hits array
+    
+    # shape N x (x, y, z, t, q, ...)
+    single_hits = np.empty(hits.shape + (9,), dtype=dtype)
+    string_idx = hits['key']['string'] - 1
+    om_idx = hits['key']['om'] - 1
+
+    single_hits[:, 0:3] = geo[string_idx, om_idx]
+    single_hits[:, 3] = hits['pulse']['time']
+    single_hits[:, 4] = hits['pulse']['charge']
+    single_hits[:, 5] = hits['pulse']['flags'] & 1 # is LC or not?
+    single_hits[:, 6] = (hits['pulse']['flags'] & 2) / 2 # has ATWD or not?
+    single_hits[:, 7] = string_idx
+    single_hits[:, 8] = om_idx
+    
+    params = get_params(labels, mcprimary, mctree, mctree_idx)
+
+    # repeats = (hits_idx['stop'] - hits_idx['start']).astype(np.int64)
+    # repeated_params = np.repeat(params, repeats=repeats, axis=0)
+
+    
+    return single_hits, params, labels
+
+def load_events(dir='/home/iwsatlas1/peller/work/oscNext/level7_v01.04/140000_i3cols',
+              labels=['x', 'y', 'z', 'time', 'azimuth','zenith', 'cascade_energy', 'track_energy', 'neutrino_energy'],
+              # geo=pkg_resources.resource_filename('freedom', 'resources/geo_array.npy'),
+              geo='./resources/geo_array.npy',
+              recos = {},
+              dtype=np.float32):
+    """
+    Create event=by=event data for hit and charge net
+    
+    Returns:
+    --------
+    list of:
+        single_hits : ndarray
+            shape (N_hits, 9)
+        total_charge : float
+        params : ndarray
+            shape (len(labels))
+    labels
+    """
+    
+    hits_idx = np.load(os.path.join(dir, 'SRTTWOfflinePulsesDC/index.npy'))
+    
+    single_hits, params, labels = load_hits(dir=dir, labels=labels, geo=geo, dtype=dtype)
+
+    # _, params, _ = load_charges(dir=dir, labels=labels, dtype=dtype)
+
+    # total_charge, params, labels = load_charges(dir=dir, labels=labels, dtype=dtype)
+
+    # string_charges, _, _ = load_strings(dir=dir, labels=labels, geo=geo, dtype=dtype)
+
+    # string_charges = string_charges.reshape(len(total_charge), 86, -1)
+
+    reco_params = {}
+    for r,f in recos.items():
+        reco = np.load(os.path.join(dir, f, 'data.npy'))
+        reco_params[r] = np.zeros_like(params)
+        for i, label in enumerate(labels):
+            if label == 'x': reco_params[r][:, i] = reco['pos']['x']
+            elif label == 'y': reco_params[r][:, i] = reco['pos']['y']
+            elif label == 'z': reco_params[r][:, i] = reco['pos']['z']
+            elif label == 'time': reco_params[r][:, i] = reco['time']
+            elif label == 'azimuth': reco_params[r][:, i] = reco['dir']['azimuth']
+            elif label == 'zenith': reco_params[r][:, i] = reco['dir']['zenith']
+            elif label == 'energy': reco_params[r][:, i] = reco['energy']
+        
+        # for retro unfortunately these are in different keys...
+        if 'track_energy' in labels and f.strip('/').endswith('__neutrino'):
+            reco_track = np.load(os.path.join(dir, f.replace('__neutrino','__track'), 'data.npy'))
+            idx = labels.index('track_energy')
+            reco_params[r][:, idx] = reco_track['energy']
+
+        if 'cascade_energy' in labels and f.strip('/').endswith('__neutrino'):
+            reco_track = np.load(os.path.join(dir, f.replace('__neutrino','__cascade'), 'data.npy'))
+            idx = labels.index('cascade_energy')
+            reco_params[r][:, idx] = reco_track['energy']
+            
+
+            
+    events = []
+    
+    for i, _ in enumerate(params):
+        event = {}
+        # event['total_charge'] = total_charge[i]
+        event['hits'] = single_hits[hits_idx[i]['start'] : hits_idx[i]['stop']]
+        event['params'] = params[i]
+        # event['strings'] = string_charges[i]
+        for r in recos.keys():
+            event[r] = reco_params[r][i]
+        events.append(event)
+    return events, labels
