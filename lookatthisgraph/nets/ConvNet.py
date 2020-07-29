c46d9c95aef71c171d3e31e0b643f2e55fe74812
Martin Ha Minh <haminh@mpp.mpg.de>
2020-07-29 15:54:57 +0200

    Allow variable input dimension in ConvNet

--- lookatthisgraph/nets/ConvNet.py
+++ lookatthisgraph/nets/ConvNet.py
@@ -7,10 +7,10 @@ from torch.nn import BatchNorm1d, PReLU
 import torch_geometric.nn as NN
 
 class ConvNet(torch.nn.Module):
-    def __init__(self, n_labels, classification=False):
+    def __init__(self, n_features, n_labels, classification=False):
         super(ConvNet, self).__init__()
         self.classification = classification
-        self.n_features = 5
+        self.n_features = n_features
         self.n_labels = n_labels
         n_intermediate = 128
         n_intermediate2 = 6*n_intermediate
