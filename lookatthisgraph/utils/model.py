35e082d18829bf8a5e5b24864d2eabdd2b1ed316
Martin Ha Minh <haminh@mpp.mpg.de>
2020-07-29 15:53:31 +0200

    Use new evaluate function in Model

--- lookatthisgraph/utils/model.py
+++ lookatthisgraph/utils/model.py
@@ -41,16 +41,15 @@ class Model:
         data_list = dataset.data_list
         n_rest = len(data_list) % batch_size
         loader = DataLoader(data_list[:-n_rest], batch_size=batch_size)
-        pred, truth = evaluate(self.model, loader, self._device)
-        pred = pred.reshape((-1, self._target_dim))
-        truth = truth.reshape((-1, self._target_dim))
+        pred = evaluate(self.model, loader, self._device)
+        pred = np.squeeze(pred.reshape(-1, self._target_dim))
 
         if evaluate_all:
             rest_loader = DataLoader(data_list[-n_rest:], batch_size=n_rest)
-            pred_rest, truth_rest = evaluate(self.model, rest_loader, self._device)
-            pred_rest = pred_rest.reshape((-1, self._target_dim))
-            truth_rest = truth_rest.reshape((-1, self._target_dim))
+            pred_rest = evaluate(self.model, rest_loader, self._device)
+            pred_rest = np.squeeze(pred_rest.reshape(-1, self._target_dim))
 
             pred = np.concatenate([pred, pred_rest])
-            truth = np.concatenate([truth, truth_rest])
+        truth = np.array([np.array(d.y) for d in data_list])[:len(pred)]
+        truth = {key: truth[:, cols] for key, cols in dataset.truth_cols.items()}
         return pred, truth
